f741ee762ece23fd165a744210d05bf3228921af
nayarocha <nayaarocha@gmail.com>
2016-01-25 16:42:48 -0300

    ajustes na responsividade - menu e cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require social-share-button
 //= require bootstrap
 //= require turbolinks
-//= require_tree 
+// require_tree
 //= require materialize-sprockets
-
+//= require init
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
 	before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  respond_to :json
+  respond_to :html, :json
   # GET /events
   # GET /events.json
   def index
